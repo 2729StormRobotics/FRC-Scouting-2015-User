854f5e9c1d2feb556cc83e7d1f2ef1d21f18c009
Garrett Beatty <gman9732@gmail.com>
2015-01-17 14:15:02 -0500

    added idiot proof thing

--- app/src/main/java/com/example/frcscouting2015/MainActivity.java
+++ app/src/main/java/com/example/frcscouting2015/MainActivity.java
@@ -138,6 +138,7 @@ public class MainActivity extends Activity {
             }*/
             EventBus.getDefault().postSticky(teamData);
             startActivity(intent);
+            finish();
         } else {
             Toast.makeText(this, "Please enter all the team's information.", Toast.LENGTH_SHORT).show();
         }
